5d74e0624e77ecc0967a87ba19176e4e91673674
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-06 11:51:15 -0300

    Feat:  Estrutura do projeto - Configurando ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,11 +8,10 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false){
-  console.log('😡 Invalid environment variables', _env.error.format());
-  
+if (_env.success === false) {
+  console.log('😡 Invalid environment variables', _env.error.format())
+
   throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
-
