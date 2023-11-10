bfe3a357aed680d570aaf4118cdceaea74df68c7
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-10 11:11:03 -0300

    Feat:  Implementando casos de uso - Caso de uso de perfil

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,6 +2,16 @@ import { UsersRepository } from '@/repositories/users-repository'
 import { User, Prisma } from '@prisma/client'
 
 export class InMemoryUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   public items: User[] = []
 
   async findByEmail(email: string) {
