e23dc93137e6d5a9b3200f593fbe4ccd1f45cdbc
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-14 15:27:57 -0300

    Feat:  Controllers & Testes E2E - Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
 import { profile } from './controllers/profile'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -10,5 +11,5 @@ export async function appRoutes(app: FastifyInstance) {
 
   /** Authenticated */
 
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
