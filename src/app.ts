358217c444c309540a5f9f572e1ca1cfaf076ce4
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-17 11:30:05 -0300

    Feat:  Refresh Token & RBAC - Implementação do refresh token

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
-    cookieName: 'refreshName',
+    cookieName: 'refreshToken',
     signed: false,
   },
   sign: {
